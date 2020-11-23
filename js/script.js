d2e81f97ecdfd387478b1214971a31bde5f73ead
Pamela Ladu <pamela.ladu@gmail.com>
2020-11-23 17:18:47 +0100

    milestone2

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// milestone2 ---> chat differenziate:
+// cliccando sulla chat vedo quella corrispondente;
+// con header con dati relativi a chat attiva;
+// relativa chat in elenco rimane selezionata
+var app = new Vue({
+el: '#container',
+data: {
+  indexChat: 0,
+  chat: [
+    {
+      name: "Fabio",
+      img: "img/avatar_1.jpg",
+      message: [
+        {
+          text: "Ciao come stai?",
+          status: "item-send",
+          day: "23/11/2020 15:32:00"
+        },
+        {
+          text: "bene tu?",
+          status: "item-received",
+          day: "23/11/2020 15:35:20",
+        }
+      ],
+    },
+    {
+      name: "Michele",
+      img: "img/avatar_2.jpg",
+      message: [
+        {
+          text: "Ricordati di dare da mangiare al gatto",
+          status:"item-send",
+          day: "22/11/2020 09:15:29"
+        },
+
+        {
+          text: "Va bene",
+          status: "item-received",
+          day: "22/11/2020 09:20:29"
+        },
+        {
+          text: "Dove trovo i croccanti?",
+          status: "item-received",
+          day: "22/11/2020 09:37:40"
+        }
+      ],
+    },
+    {
+      name: "Valeriana",
+      img: "img/avatar_6.jpg",
+      message: [
+        {
+          text: "Hai visto Sucker Punch?",
+          status:"item-send",
+          day: "21/11/2020 23:30:01"
+        },
+        {
+          text: "Si! Tu che ne pensi?",
+          status: "item-received",
+          day: "21/11/2020 23:45:37"
+        }
+      ],
+    },
+    {
+      name: "Luisa",
+      img: "img/avatar_4.jpg",
+      message: [
+        {
+          text: "Ciao ci vediamo sta sera?",
+          status:"item-received",
+          day: "21/11/2020 21:00:00"
+        },
+        {
+          text: "Mmm non so hai qualcosa di strano",
+          status: "item-send",
+          day: "21/11/2020 21:30:30"
+        }
+      ],
+    }
+
+  ]
+
+},
+methods: {
+  // funzione per collegare chat selezionata al blocco chat aperta
+    selectcontact(index) {
+      this.indexChat = index;
+    },
+  },
+});
